2655484e003ab4245d35fdbac11bb0e23e4851d8
soapblox <soapblox@gmail.com>
2020-02-22 13:17:18 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+var currentState = "";
+var currentPlayerState = "";
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+server.listen(port, () => {
+    console.log('Server listening at port %d', port);
+  });
+
+app.get('/panel', function (req, res) {
+    console.log("loading panel...");
+    res.sendFile(__dirname + '/panel.html');
+});
+
+app.get('/playerWidget', function (req, res) {
+    console.log("loading playerWidget...");
+    res.sendFile(__dirname + '/playerWidget.html');
+});
+
+app.get('/eventWidget', function (req, res) {
+    console.log("loading eventWidget...");
+    res.sendFile(__dirname + '/eventWidget.html');
+});
+
+app.get('/activeGameWidget', function (req, res) {
+    console.log("loading activeGameWidget...");
+    res.sendFile(__dirname + '/activeGameWidget.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+    socket.on('paint', function (msg) {
+        console.log('Index.js-paint');
+        io.emit('paint', currentState);
+    });
+
+
+    socket.on('panelToPlayerUpdate', function (msg) {
+        console.log('Index.js-paint');
+        io.emit('playerStateUpdate', msg);
+    });    
+
+    socket.on('panelToActiveGameUpdate', function (msg) {
+        console.log('Index.js-paint');
+        io.emit('activeGameStateUpdate', msg);
+    });
+
+    
+
+    socket.on('startPlayerWidget', function (msg) {
+        console.log('Inside startPlayerWidget... ');
+        //io.emit('paint', currentState);
+    });
+
+    socket.on('startPanel', function (msg) {
+        console.log('Inside startPanel... ');
+    });    
+
+    socket.on('setCurrentPlayer', function (msg) {
+        console.log(msg);
+        io.emit('paintPlayer', msg);
+    });
+
+    socket.on('setActiveGames', function (msg) {
+        console.log(msg);
+        io.emit('paintActiveGames', msg);
+    });
+
+    socket.on('pinEvents', function (msg) {
+        console.log(msg);
+        io.emit('updatePinEvents', msg);
+    });
+
+});
---
+++ public/marquee.js
@@ -0,0 +1,87 @@
+function interval(duration, fn) {
+    this.baseline = undefined
+
+    this.run = function(){
+        if(this.baseline === undefined){
+        this.baseline = new Date().getTime()
+        }
+        fn()
+        var end = new Date().getTime()
+        this.baseline += duration
+    
+        var nextTick = duration - (end - this.baseline)
+        if(nextTick<0){
+        nextTick = 0
+        }
+        (function(i){
+            i.timer = setTimeout(function(){
+            i.run(end)
+        }, nextTick)
+        }(this))
+    }
+
+    this.stop = function(){
+        clearTimeout(this.timer)
+    }
+}
+
+class Player {
+    constructor(id, name) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+class Match {
+    constructor() {
+        this.id = "";
+        this.state = "";
+        this.player1; // player object
+        this.player2; // player object
+        this.player1Score;
+        this.player2Score;
+        this.round;
+        this.matchScore;
+    }
+}
+
+class PinMarquee {
+    constructor() {
+        this.players = [];
+        this.women = []
+        this.matches = [];
+        this.womensMatches = [];
+    }
+
+    getMatchesByPlayerId(pId) {
+        var playerMatches = [];
+        //console.log(pId);
+        for (var i = 0; i < this.matches.length; i++) {
+            console.log(this.matches[i].player1);
+            console.log(this.matches[i].player2);
+            if (typeof this.matches[i].player1 === 'undefined' || typeof this.matches[i].player2 === 'undefined') {
+                console.log("undefined players on match..");
+            }
+            else if (this.matches[i].player1.id == pId || this.matches[i].player2.id == pId) {
+                //console.log("found player match...");
+                //console.log(matches[i]);
+                playerMatches.push(this.matches[i]);
+            }
+        }
+
+        return playerMatches;
+    }
+
+    getPlayerById(pId) {
+        // match doesn't know what player it is yet, so ignore...
+        if (pId == null) {
+           return;
+       }
+       console.log(this.players.length);
+       for (var i = 0; i < this.players.length; i++) {
+           if (this.players[i].id == pId) {
+               return this.players[i];
+           }
+       }
+    }
+}
